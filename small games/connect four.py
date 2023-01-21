5dec261a2b9a785a6a48d0868e79bbff0c3d03b2
Johnny Phan <pjohnny550@gmail.com>
2023-01-21 14:40:01 -0800

    built player 1 and player 2 input to board

--- small games/connect four.py
+++ small games/connect four.py
@@ -1,25 +1,48 @@
 import numpy as np
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
 
 def create_board():
     board = np.zeros((6,7))
     return board
 
-def drop_piece():
-    pass
-def valid_location():
-    pass
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+def valid_location(board, col):
+    return board[5][col] == 0
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def print_board(board):
+    print(np.flip(board,0))
 
 board = create_board()
+print_board(board)
 game_over = False
 turn = 0
 
 while not game_over:
     #asks for player 1 input
      if turn == 0:
-         selection = int(input("Player 1 make your selection (0-6): "))
+         col = int(input("Player 1 make your selection (0-6): "))
+
+         if valid_location(board,col):
+             row = get_next_open_row(board,col)
+             drop_piece(board,row,col,1)
+
          #asks for player 2 input
      else:
-         selection = int(input("Player 2 make your selection (0-6): "))
+         col = int(input("Player 2 make your selection (0-6): "))
+
+         if valid_location(board,col):
+             row = get_next_open_row(board,col)
+             drop_piece(board,row,col,2)
+
+     print_board(board)
+
      turn += 1
      turn = turn % 2
 
